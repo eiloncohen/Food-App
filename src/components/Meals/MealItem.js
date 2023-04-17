a00755207d04f907df8aeae6d41fedf8f0d82601
eilon <eiloncohen20@gmail.com>
2023-04-17 09:57:35 +0300

    add cart component and fix id bug in mealItemForm component

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -5,14 +5,14 @@ import MealItemForm from "./MealItemForm"
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`
     return(
-    <li key={props.key} className={styles.meal}>
+    <li key={props.id} className={styles.meal}>
         <div>
             <h3>{props.name}</h3>
             <div className={styles.description}>{props.description}</div>
             <div className={styles.price}>{price}</div>
         </div>
         <div>
-            <MealItemForm/>
+            <MealItemForm id={props.id}/>
         </div>
     </li>
     )
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -4,12 +4,12 @@ import Input from '../UI/Input'
 const MealItemForm = props => {
 return <form className={styles.form}>
     <Input label="Amount" input={{
-        id: 'amount',
+        id: 'amount_' + props.id,
         type: 'number',
         min: '1',
         max: '5',
         step: '1',
-        defultValue: '1'
+        defaultValue: '1'
     }}/>
     <button>+ Add</button>
 </form>
