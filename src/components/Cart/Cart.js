feb373d454c5e4784e7321a3061d6117ada56cca
eilon <eiloncohen20@gmail.com>
2023-04-18 15:08:27 +0300

    Managing Cart & Modal State

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,24 +5,24 @@ const Cart = props => {
     const cartItems = <ul className={styles['cart-items']}>
         {
         [{
-                id: 'c1',
-                name: 'Sushi',
-                amout: 2,
-                price: 12.99
+            id: 'c1',
+            name: 'Sushi',
+            amout: 2,
+            price: 12.99
         }].map(item=> <li>
             {item.name}
         </li>)
         }
     </ul>
     return (
-    <Modal>
+    <Modal onHideCart={props.onHideCart}>
         {cartItems} 
         <div className={styles.total}>
             <span>Total Amount</span>
             <span>35.62</span>
         </div>
         <div className={styles.actions}>
-            <button className={styles['button--alt']}>Close</button>
+            <button className={styles['button--alt']} onClick={props.onHideCart}>Close</button>
             <button className={styles.button}>Order</button>
         </div>
     </Modal>
