387e30e7dec404b82609eb13b40c56937988522e
eilon <eiloncohen20@gmail.com>
2023-04-27 11:30:19 +0300

    add bump css class to cart button every time when items array in cart is change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,42 @@
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=> {
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item)=> {
         return curNumber + item.amount;
     }, 0);
 
+    // good way to add css styles by checking if variable is true or false
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump: ''}`
+
+    // use in useEffect to target changes in items on the cart and when we recognize a change, we add a "bump" css class to cart button.
+    useEffect(() => {
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        // remove the css class "bump" because we want to see the "bump" css class every time we add item to cart, 
+        //and not only at the first time we add.
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
     return( 
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
         <span className={styles.icon}>
             <CartIcon />
         </span>
